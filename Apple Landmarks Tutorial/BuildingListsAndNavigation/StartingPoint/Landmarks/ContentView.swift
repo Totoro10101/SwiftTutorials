6943175eeb829ce58fc9295c0358b36dfccc52c0
Zachary <67016573+Totoro10101@users.noreply.github.com>
2022-03-23 20:35:59 -0700

    Completed first tutorial creating and combining views

--- Apple Landmarks Tutorial/BuildingListsAndNavigation/StartingPoint/Landmarks/ContentView.swift
+++ Apple Landmarks Tutorial/BuildingListsAndNavigation/StartingPoint/Landmarks/ContentView.swift
@@ -1,9 +1,9 @@
-//
-//  ContentView.swift
-//  Landmarks
-//
-//  Created by Zachary Stednitz on 3/23/22.
-//
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+A view showing the details for a landmark.
+*/
 
 import SwiftUI
 
@@ -13,31 +13,31 @@ struct ContentView: View {
             MapView()
                 .ignoresSafeArea(edges: .top)
                 .frame(height: 300)
-            
+
             CircleImage()
                 .offset(y: -130)
                 .padding(.bottom, -130)
-            
+
             VStack(alignment: .leading) {
                 Text("Turtle Rock")
                     .font(.title)
+
                 HStack {
                     Text("Joshua Tree National Park")
-                        .font(.subheadline)
                     Spacer()
                     Text("California")
-                        .font(.subheadline)
                 }
                 .font(.subheadline)
                 .foregroundColor(.secondary)
+
                 Divider()
-                
+
                 Text("About Turtle Rock")
                     .font(.title2)
-                Text("Descriptive text goes here")
+                Text("Descriptive text goes here.")
             }
             .padding()
-            
+
             Spacer()
         }
     }
